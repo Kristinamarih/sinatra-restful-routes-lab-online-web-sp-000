c11df52b2b86f176409cdd29cc0e305883343f67
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-16 20:43:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
         ingredients: params[:ingredients],
         cook_time: params[:cook_time]
         )
-    redirect to '/recipes/#{@recipe.id}'
+    redirect '"recipes/#{recipe.id}"
     end
   end
   
